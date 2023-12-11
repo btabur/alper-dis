a55c46c03b68b8c418f17ecf7fa05768c0115265
btabur <webtasarim.0323@gmail.com>
2023-12-11 22:47:32 +0300

    added about section

---
+++ src/componenets/About.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <section className="about">
+        <article className="left">
+        <h3>Hakkımızda</h3>
+        <h2>Biz Kimiz?</h2>
+        <p>Alper Karabağ Diş Sağlığı Merkezi, Kalite, Çevre ve Güvenlik alanında 3 ISO Sertifikasına sahip ilk özel sağlık şirketidir. Hastalara sunulan hizmetlerin kalitesi üçüncü taraf kuruluşlar tarafından onaylanmıştır ve yenilik ve eğitim, mesleğin gelişiminde temel bir rol oynamaktadır.</p>
+        <div className='hour'>
+            <div className='background'>
+                <img src="./public/icons/clock.png" />
+            </div>
+            <div className='info'>
+                <p>Herkes için uygun saatler</p>
+                <span>Sabah 9'dan akşam 8'e kadar sürekli olarak açıktır.</span>
+            </div>
+           
+        </div>
+        <div className='hour'>
+            <div className='background'>
+            <img src="./public/icons/icon-5.png" />
+            </div>
+            <div className='info'>
+            <p>Ödeme şartları</p>
+            <span>Kişiye özel ödeme planı sunuyoruz. Hastanın sağlığı her zaman ilk hedefimiz olmalıdır.</span>
+            </div>
+          
+        </div>
+        </article>
+        <article className="right">
+            <div className="background"></div>
+            <img src="./public/about.png" alt="" />
+        </article>
+      
+    </section>
+  )
+}
+
+export default About
--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import Banner from '../componenets/Banner'
 import Services from '../componenets/Services'
+import About from '../componenets/About'
 
 const MainPage = () => {
   return (
     <div>
       <Banner/>
       <Services/>
+      <About/>
     </div>
   )
 }
